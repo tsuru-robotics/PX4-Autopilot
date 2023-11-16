a3b4b9b0755b3117ed77a0b0c1eda1d7113a6622
vmsav <vms@flyfire.io>
2023-11-16 20:36:15 +0300

    UTM->2.5, GPS_RAW->1

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1693,11 +1693,12 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 
 	/* fallthrough */
 	case MAVLINK_MODE_CUSTOM:
-		configure_stream_local("UTM_GLOBAL_POSITION", 5.0f);
 		configure_stream_local("SYS_STATUS", 1.0f);
-		configure_stream_local("GPS_RAW_INT", 0.25f);
+		configure_stream_local("GPS_RAW_INT", 1.0f);
 		configure_stream_local("GLOBAL_POSITION_INT", 0.25f);
 		configure_stream_local("ATTITUDE", 0.2f);
+		configure_stream_local("UTM_GLOBAL_POSITION", 2.5f);
+
 		break;
 
 	case MAVLINK_MODE_CONFIG: // USB
